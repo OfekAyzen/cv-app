ce0e53d18f56d320f6a7b8e98c21cfca531fec8a
kristinatech19 <Kristina.Tsivilev@tech-19.com>
2023-08-02 10:19:51 +0300

    FRONT

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+import React from 'react';
+import { Tabs, Tab, AppBar, Box, FormControlLabel, Checkbox, FormGroup, Grid, colors } from '@mui/material';
+import logo from '../Components/images/logo_tech19.png';
+
+const Header = () => {
+    return(
+        <>
+            <div className='div-bar' position="static" >
+             <img src={logo} />
+            </div>
+        
+        </>
+    )
+};
+
+export default Header;
