57eb22efea7a3d0fa89195d0c1c68d57a0a5ea5e
kristinatech19 <132443670+kristinatech19@users.noreply.github.com>
2023-09-05 10:11:48 +0300

    remove python venv

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,10 @@
-
+import os
 
 # Define the database connection settings
-DB_USERNAME = 'postgres'
-DB_PASSWORD = 'Tech1919!'
-DB_HOST = 'dev.cstxeaoeb5yt.us-east-2.rds.amazonaws.com'
-DB_NAME = 'onlinepostgres'
+DB_USERNAME = os.environ.get('DB_USERNAME' , 'postgres')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
 
 # Construct the database URI
 SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
