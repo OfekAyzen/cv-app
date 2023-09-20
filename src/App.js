0063419eb423ca51a9306657408ffc2b71b5ec6f
kristinatech19 <132443670+kristinatech19@users.noreply.github.com>
2023-09-20 13:56:40 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState,useEffect } from 'react';
+import { BrowserRouter , Route, Routes, Navigate, Outlet, Link } from 'react-router-dom';
+
+import { Amplify } from '@aws-amplify/core';
+
+
+import '@aws-amplify/ui-react/styles.css';
+
+import Login from './Pages/Log/Login';
+
+// import Logout from './Pages/Log/Logout'
+// import useToken from './useToken';
+// import ProfileManager from "./Pages/Manager/ProfileManager";
+// import ProfileUser from './Components/Users/ProfileUser';
+
+// import JobApplication from './Components/Users/JobApplication';
+// import Position from './Pages/Manager/Position';
+// // import NotFound from './NotFound';
+// import SignUp from '../src/Components/SignUp/SignUp';
+// import ForgotPassword from './Components/Users/ForgotPassword';
+// import ConfirmSignUp from './Pages/Log/ConfirmSignUp';
+// import ConfirmForgotPassword from './Pages/Log/ConfirmForgotPassword';
+// import {  Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+
 
 function App() {
+ 
+  const [candidateData, setCandidateData] = useState([]);
+
+  const handleJobApplication = (applicationData) => {
+    setCandidateData([...candidateData, applicationData]);
+  };
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <BrowserRouter>
+    <div style={{width:'100%'}}>
+       
+        <Routes>
+        
+            <Route path="/Login" element={<Login />} />
+      
+              {/* <Route path="/" element={<Navigate to="/Login" replace />} />
+              <Route path="/Profile" element={<ProfileManager  />} />
+            
+              <Route path="/HomePage" element={<ProfileUser />} />
+              <Route path="/Apply/:job_id"  element={<JobApplication/>}/>
+              <Route path="/Position" element={<Position  />} />
+
+              <Route path="/confirm-signup/:username" element={<ConfirmSignUp/>} />
+              <Route path="/confirm_forgot_password/:username" element={<ConfirmForgotPassword/>} />
+              <Route path="/SignUp" element={<SignUp></SignUp>} />
+              <Route path="/Logout" element={<Logout />} /> */}
+            {/* <Route path="/HomePage" element={<ProfileUser username={username} onApplicationSubmit={handleJobApplication} candidate_id={candidateId} token={token} userRole={userRole} setToken={setToken} />} /> */}
+            {/* <Route path="/forgot_password" element={<ForgotPassword/>} />
+            <Route path="*" element={<Navigate to="/Login" replace />} /> */}
+
+        </Routes>
     </div>
+
+  </BrowserRouter>
+  
+  </>
+
   );
 }
 
---
+++ src/Pages/Log/Login.js
@@ -0,0 +1,185 @@
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import logo from '../../../src/Components/images/logo_tech19.png';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Header from "../../Components/Header";
+import "../../styles/LoginPage.css";
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import { Link as RouterLink } from 'react-router-dom'; // Import RouterLink
+import { Link } from '@mui/material';
+
+const defaultTheme = createTheme();
+
+export default function Login(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState('');
+
+  const navigate = useNavigate();
+
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    if (username === '') {
+      console.log('Login Failed');
+      setLoginError('Please enter a username');
+      return;
+    }
+    if (password === '') {
+      console.log('Login Failed');
+      setLoginError('Please enter a password');
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://localhost:5000/token', {
+        username: username,
+        password: password,
+      });
+
+      if (response.status === 200) {
+        // Login successful
+
+        props.onLogIn(response.data.role);
+        props.setToken(response.data.access_token)
+       
+        props.setUserName(response.data.username);
+       
+
+        const pendingJobId = sessionStorage.getItem('pendingJobId');
+        if (pendingJobId) {
+         
+
+          localStorage.setItem('candidate_id', response.data.candidate_id);//
+          navigate(`/Apply/${pendingJobId}`);
+          sessionStorage.removeItem('pendingJobId');
+        } else {
+          // No pending job ID, navigate to the appropriate page based on user role
+          if (response.data.role === 'candidate') {
+            localStorage.setItem('candidate_id', response.data.candidate_id);//
+           
+            props.setCandidateId(response.data.candidate_id);
+            navigate('/HomePage');
+          } else if (response.data.role === 'manager') {
+            props.setUserId(response.data.user_id);
+            navigate('/Profile');
+          }
+        }
+      } else {
+        // Login failed
+        setLoginError('Incorrect username or password');
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      }
+
+      setLoginError('Error logging in');
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div className='div-bar' position="static" >
+        <img src={logo} />
+      </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h4">
+            Welcome back !
+          </Typography>
+          <Typography component="h1" variant="h6">
+            Welcome back! Please enter your details.
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, textAlign: 'center', }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              value={username}
+              autoFocus
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 4, mb: 2, width: '30%', color: 'white', backgroundColor: '#ad2069','&:hover': {
+                backgroundColor: '#b4269a', // Set the hover background color
+              }, }}  // Center-align the login button
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item xs={6}> {/* Change the xs value */}
+                <Link component={RouterLink} to="/forgot_password" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item xs={6}> {/* Change the xs value */}
+                <Link component={RouterLink} to="/SignUp" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
