6b39e8d16a1624f531aaef3d4d68610bebaf4592
OfekAyzen <103589176+OfekAyzen@users.noreply.github.com>
2024-01-25 13:18:06 +0200

    line fix 1

--- src/Pages/Manager/CandidateTable.js
+++ src/Pages/Manager/CandidateTable.js
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect } from "react";
 import {
   TableRow,
@@ -23,67 +21,75 @@ import Snackbar from "@mui/material/Snackbar";
 import TablePagination from "@mui/material/TablePagination";
 import MuiAlert from "@mui/material/Alert";
 import AddCandidateForm from "./AddCandidateForm";
-import Dialog from '@mui/material/Dialog';
+import Dialog from "@mui/material/Dialog";
 
-const CandidateRow = ({ candidate,
-  appliedJob, selectedJobId, handleViewCandidate,
-  handleDeleteApplication, onDeleteCandidate, }) => {
-    const [isRowClicked, setIsRowClicked] = useState(false);
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState('');
-  
-    const handleDelete = async (deletedCandidateId, candidateVersion) => {
-      const confirmed = window.confirm('Are you sure you want to delete this candidate?');
-  
-      if (!confirmed) {
-        return;
+const CandidateRow = ({
+  candidate,
+  appliedJob,
+  selectedJobId,
+  handleViewCandidate,
+  handleDeleteApplication,
+  onDeleteCandidate,
+}) => {
+  const [isRowClicked, setIsRowClicked] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const handleDelete = async (deletedCandidateId, candidateVersion) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this candidate?"
+    );
+
+    if (!confirmed) {
+      return;
+    }
+
+    try {
+      const input = {
+        id: deletedCandidateId,
+        _version: candidateVersion, // Use the correct version for the candidate
+      };
+
+      const response = await API.graphql(
+        graphqlOperation(deleteCandidate, { input })
+      );
+
+      if (response.data && response.data.deleteCandidate) {
+        console.log("Candidate deleted successfully");
+        setSnackbarMessage("Candidate deleted successfully");
+        setSnackbarOpen(true);
+        setTimeout(() => {
+          setSnackbarOpen(false);
+        }, 10000);
+        window.location.reload();
+      } else {
+        handleNotification("Error deleting candidate", "error");
       }
-  
-      try {
-        const input = {
-          id: deletedCandidateId,
-          _version: candidateVersion, // Use the correct version for the candidate
-        };
-  
-        const response = await API.graphql(graphqlOperation(deleteCandidate, { input }));
-  
-        if (response.data && response.data.deleteCandidate) {
-          console.log('Candidate deleted successfully');
-          setSnackbarMessage('Candidate deleted successfully');
-          setSnackbarOpen(true);
-          setTimeout(() => {
-            setSnackbarOpen(false);
-          }, 10000);
-          window.location.reload();
-        } else {
-          handleNotification('Error deleting candidate', 'error');
-        }
-      } catch (error) {
-        console.error('Error deleting candidate:', error);
-        handleNotification('Error deleting candidate', 'error');
-      }
-    };
-  
+    } catch (error) {
+      console.error("Error deleting candidate:", error);
+      handleNotification("Error deleting candidate", "error");
+    }
+  };
 
   // const [isRowClicked, setIsRowClicked] = useState(false);
 
   // const handleDelete = async (deletedCandidateId, candidateVersion) => {
   //   const confirmed = window.confirm("Are you sure you want to delete this candidate?");
-  
+
   //   if (!confirmed) {
   //     return;
   //   }
-  
+
   //   try {
   //     const input = {
   //       id: deletedCandidateId,
   //       _version: candidateVersion, // Use the correct version for the candidate
   //     };
-  
+
   //     const response = await API.graphql(
   //       graphqlOperation(deleteCandidate, { input })
   //     );
-  
+
   //     if (response.data && response.data.deleteCandidate) {
   //       console.log("Candidate deleted successfully");
   //       window.location.reload();
@@ -102,13 +108,11 @@ const CandidateRow = ({ candidate,
 
   const formatDate = (isoDate) => {
     const date = new Date(isoDate);
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are zero-based
     const year = date.getFullYear();
     return `${day}-${month}-${year}`;
   };
-
-
 
   const handleRowClick = () => {
     if (candidate.job && candidate.job.id) {
@@ -122,48 +126,60 @@ const CandidateRow = ({ candidate,
   const handleNotification = (message, type) => {
     // Implement your notification logic here
   };
-  
+
   return (
-
-
     <TableRow
-    key={candidate.candidate.id}
-    sx={{
-      borderRadius: "10px",
-      transition: "box-shadow 0.3s",
-      boxShadow: isRowClicked ? "0px 0px 5px 3px #ad2069" : "none",
-    
-      "&:hover": {
-        boxShadow: "0px 0px 5px 3px #d166b1",
-        cursor: "pointer",
-      },
-    }}
-    onClick={handleRowClick}
-  >
-      <TableCell  className="my-font">{formatDate(candidate.candidate.createdAt)}</TableCell>
-      <TableCell   className="my-font">{candidate.candidate.first_name} {candidate.candidate.last_name}</TableCell>
-      <TableCell   className="my-font">{candidate.candidate.location}</TableCell>
-      <TableCell    className="my-font">{candidate.candidate.gender}</TableCell>
-      <TableCell   className="my-font">
-        {candidate.job?.job_title || candidate.candidate.position || 'No job title'}
+      key={candidate.candidate.id}
+      sx={{
+        borderRadius: "10px",
+        transition: "box-shadow 0.3s",
+        boxShadow: isRowClicked ? "0px 0px 5px 3px #ad2069" : "none",
+
+        "&:hover": {
+          boxShadow: "0px 0px 5px 3px #d166b1",
+          cursor: "pointer",
+        },
+      }}
+      onClick={handleRowClick}
+    >
+      <TableCell className="my-font">
+        {formatDate(candidate.candidate.createdAt)}
       </TableCell>
-      <TableCell    className="my-font">{candidate.candidate.education}</TableCell>
-      <TableCell    className="my-font">{candidate.candidate.work_experience}</TableCell>
-      <TableCell   className="my-font">{candidate.candidate.skills}</TableCell>
-      <TableCell   className="my-font">{candidate.candidate.status}</TableCell>
+      <TableCell className="my-font">
+        {candidate.candidate.first_name} {candidate.candidate.last_name}
+      </TableCell>
+      <TableCell className="my-font">{candidate.candidate.location}</TableCell>
+      <TableCell className="my-font">{candidate.candidate.gender}</TableCell>
+      <TableCell className="my-font">
+        {candidate.job?.job_title ||
+          candidate.candidate.position ||
+          "No job title"}
+      </TableCell>
+      <TableCell className="my-font">{candidate.candidate.education}</TableCell>
+      <TableCell className="my-font">
+        {candidate.candidate.work_experience}
+      </TableCell>
+      <TableCell className="my-font">{candidate.candidate.skills}</TableCell>
+      <TableCell className="my-font">{candidate.candidate.status}</TableCell>
       <TableCell className="fullWidthCell">
         <Button
           variant="contained"
           color="secondary"
           style={{ fontFamily: '"Calibri", sans-serif' }}
-          onClick={() => handleDelete(candidate.candidate.id, candidate.candidate._version)}
+          onClick={() =>
+            handleDelete(candidate.candidate.id, candidate.candidate._version)
+          }
         >
           Delete
         </Button>
       </TableCell>
 
       {/* Snackbar to display delete message */}
-      <Snackbar open={snackbarOpen} autoHideDuration={5000} onClose={() => setSnackbarOpen(false)}>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={5000}
+        onClose={() => setSnackbarOpen(false)}
+      >
         <MuiAlert
           elevation={6}
           variant="filled"
@@ -176,19 +192,6 @@ const CandidateRow = ({ candidate,
     </TableRow>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const CandidateTable = ({
   candidates,
@@ -220,7 +223,6 @@ const CandidateTable = ({
     setDialogOpen(false);
   };
 
-
   // function to sort candidates based on the selected sorting criteria
   const sortCandidates = (candidates) => {
     return candidates.slice().sort((a, b) => {
@@ -245,17 +247,8 @@ const CandidateTable = ({
     });
   };
 
-
-
-
   const filteredCandidates = candidates.filter((candidate) => {
-    const {
-      education,
-      workExperience,
-      skills,
-      gender,
-      location,
-    } = filters;
+    const { education, workExperience, skills, gender, location } = filters;
 
     return (
       (education === "" || candidate.candidate.education === education) &&
@@ -280,9 +273,7 @@ const CandidateTable = ({
   const endIndex = startIndex + rowsPerPage;
   const displayedCandidates = sortedCandidates.slice(startIndex, endIndex);
 
-
   const handleChangePage = (event, newPage) => {
-
     setPage(newPage);
   };
 
@@ -292,13 +283,6 @@ const CandidateTable = ({
     setRowsPerPage(newRowsPerPage);
     setPage(0); // Reset page to 0 when rows per page changes
   };
-
-
-
-
-
-
-
 
   //SORT
 
@@ -317,7 +301,6 @@ const CandidateTable = ({
     setFilters(newFilters);
   };
 
-  
   const handleNotification = (message, type) => {
     // Set the notification state
     setNotification({ message, type });
@@ -328,22 +311,22 @@ const CandidateTable = ({
     return () => clearTimeout(timer);
   };
 
-
   const AddCandidateDialog = ({ open, handleClose }) => {
     return (
-      <Dialog 
-     
-      open={open} onClose={handleClose}>
-        
-      
-          <AddCandidateForm />
-      
+      <Dialog open={open} onClose={handleClose}>
+        <AddCandidateForm />
       </Dialog>
     );
   };
   return (
-
-    <div style={{ width: '1700px', height: '1200px',paddingLeft:'5%',paddingRight:'4%' }}>
+    <div
+      style={{
+        width: "1700px",
+        height: "1200px",
+        paddingLeft: "5%",
+        paddingRight: "4%",
+      }}
+    >
       <Snackbar
         open={notification !== null}
         autoHideDuration={5000}
@@ -359,32 +342,30 @@ const CandidateTable = ({
         </MuiAlert>
       </Snackbar>
 
-
-      <Container
-        className="candidate-table"
-    
-      >
-        <div className="container" style={{  padding: '15px',width: '140%'  }} >
+      <Container className="candidate-table">
+        <div className="container" style={{ padding: "15px", width: "140%" }}>
           <CandidateFilterForm onFilter={handleFilter} />
 
           <div className="orderby">
-            <FormControl variant="outlined" className="custom-select" sx={{width:'15.5%',paddingLeft:'5px'}}>
+            <FormControl
+              variant="outlined"
+              className="custom-select"
+              sx={{ width: "15.5%", paddingLeft: "5px" }}
+            >
               <InputLabel>Sort By:</InputLabel>
               <Select
                 value={sortByField}
                 onChange={(e) => handleSortByChange(e.target.value)}
                 label="Sort By:"
-
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value="education">Education</MenuItem>
-                <MenuItem value="skills">Skills</MenuItem>
-                <MenuItem value="gender">Gender</MenuItem>
-                <MenuItem value="location">Location</MenuItem>
-                <MenuItem value="application_date">Date</MenuItem>
-              </Select>
+              ></Select>
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="education">Education</MenuItem>
+              <MenuItem value="skills">Skills</MenuItem>
+              <MenuItem value="gender">Gender</MenuItem>
+              <MenuItem value="location">Location</MenuItem>
+              <MenuItem value="application_date">Date</MenuItem>
             </FormControl>
           </div>
         </div>
@@ -393,21 +374,19 @@ const CandidateTable = ({
           <TableHead>
             <TableRow>
               <TableCell className="my-font">Date Apply</TableCell>
-              <TableCell   className="my-font">Full Name</TableCell>
-              <TableCell  className="my-font">Location</TableCell>
-              <TableCell  className="my-font">Gender</TableCell>
-              <TableCell  className="my-font">Position</TableCell>
-              <TableCell  className="my-font">Education</TableCell>
-              <TableCell   className="my-font">Work Experience</TableCell>
-              <TableCell  className="my-font">Skills</TableCell>
-              <TableCell  className="my-font">Status</TableCell>
-              <TableCell  className="my-font">Actions</TableCell>
+              <TableCell className="my-font">Full Name</TableCell>
+              <TableCell className="my-font">Location</TableCell>
+              <TableCell className="my-font">Gender</TableCell>
+              <TableCell className="my-font">Position</TableCell>
+              <TableCell className="my-font">Education</TableCell>
+              <TableCell className="my-font">Work Experience</TableCell>
+              <TableCell className="my-font">Skills</TableCell>
+              <TableCell className="my-font">Status</TableCell>
+              <TableCell className="my-font">Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-         
             {displayedCandidates.map((candidate) => (
-             
               <CandidateRow
                 key={candidate.candidate.id}
                 candidate={candidate}
@@ -417,44 +396,51 @@ const CandidateTable = ({
               />
             ))}
           </TableBody>
-          
         </Table>
 
-     
-      
         <TablePagination
-        
-            sx={{ display: 'flex', width: '122.6%', backgroundColor: 'white'}}
-            component="div"
-            count={sortedCandidates.length} // Use sortedCandidates.length
-            page={page}
-            onPageChange={handleChangePage} // Ensure handleChangePage is called
-            rowsPerPage={rowsPerPage}
-            onRowsPerPageChange={handleChangeRowsPerPage} // Ensure handleChangeRowsPerPage is called
-            labelRowsPerPage="Rows per page:"
-            rowsPerPageOptions={[10, 25, 50]}
-          />
-          <div style={{backgroundColor:'white',width:'120%',padding:'15px',
-           borderBottomLeftRadius:'10px',borderBottomRightRadius:'15px',}}>
-          <Button
-        variant="contained"
-        sx={{fontFamily:'"Calibri", sans-serif', backgroundColor: 'grey','&:hover': {
-          backgroundColor: 'darkgrey'
-        } }}
-        onClick={handleAddCandidateClick}
-      >
-       Add Candidate
-      </Button>
-      </div>
-
-      {showAddCandidateForm && (
-        <AddCandidateDialog
-          open={dialogOpen}
-          handleClose={handleDialogClose}
+          sx={{ display: "flex", width: "122.6%", backgroundColor: "white" }}
+          component="div"
+          count={sortedCandidates.length} // Use sortedCandidates.length
+          page={page}
+          onPageChange={handleChangePage} // Ensure handleChangePage is called
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage} // Ensure handleChangeRowsPerPage is called
+          labelRowsPerPage="Rows per page:"
+          rowsPerPageOptions={[10, 25, 50]}
         />
-      )}
+        <div
+          style={{
+            backgroundColor: "white",
+            width: "120%",
+            padding: "15px",
+            borderBottomLeftRadius: "10px",
+            borderBottomRightRadius: "15px",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              fontFamily: '"Calibri", sans-serif',
+              backgroundColor: "grey",
+              "&:hover": {
+                backgroundColor: "darkgrey",
+              },
+            }}
+            onClick={handleAddCandidateClick}
+          >
+            Add Candidate
+          </Button>
+        </div>
+
+        {showAddCandidateForm && (
+          <AddCandidateDialog
+            open={dialogOpen}
+            handleClose={handleDialogClose}
+          />
+        )}
       </Container>
     </div>
   );
-}
+};
 export default CandidateTable;
