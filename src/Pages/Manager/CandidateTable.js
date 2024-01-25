169e2359c2d307aa8290ef7fd557ed0a2924b265
OfekAyzen <103589176+OfekAyzen@users.noreply.github.com>
2024-01-25 13:29:48 +0200

    line fix 2

--- src/Pages/Manager/CandidateTable.js
+++ src/Pages/Manager/CandidateTable.js
@@ -345,6 +345,20 @@ const CandidateTable = ({
       <Container className="candidate-table">
         <div className="container" style={{ padding: "15px", width: "140%" }}>
           <CandidateFilterForm onFilter={handleFilter} />
+          <Select
+            value={sortByField}
+            onChange={(e) => handleSortByChange(e.target.value)}
+            label="Sort By:"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="education">Education</MenuItem>
+            <MenuItem value="skills">Skills</MenuItem>
+            <MenuItem value="gender">Gender</MenuItem>
+            <MenuItem value="location">Location</MenuItem>
+            <MenuItem value="application_date">Date</MenuItem>
+          </Select>
 
           <div className="orderby">
             <FormControl
@@ -353,19 +367,6 @@ const CandidateTable = ({
               sx={{ width: "15.5%", paddingLeft: "5px" }}
             >
               <InputLabel>Sort By:</InputLabel>
-              <Select
-                value={sortByField}
-                onChange={(e) => handleSortByChange(e.target.value)}
-                label="Sort By:"
-              ></Select>
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value="education">Education</MenuItem>
-              <MenuItem value="skills">Skills</MenuItem>
-              <MenuItem value="gender">Gender</MenuItem>
-              <MenuItem value="location">Location</MenuItem>
-              <MenuItem value="application_date">Date</MenuItem>
             </FormControl>
           </div>
         </div>
